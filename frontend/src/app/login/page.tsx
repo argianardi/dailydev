8231b8f044e354b255813b560316b7e5061f5f89
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-28 00:30:08 +0700

    Fix: Delete unneeded components

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,15 +1,4 @@
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import Image from 'next/image';
 import React from 'react';
