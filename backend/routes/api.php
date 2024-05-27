4617a3f223c3cf5c225220d6f136d3db2acb2473
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-27 10:20:36 +0700

    Feat: Add logout route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,6 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+});
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
