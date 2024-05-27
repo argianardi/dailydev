74a2a02fbb653a2b0f72490484bc54a47d8a9e9a
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-05-28 00:13:07 +0700

    Feat: Add update profile image request

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,7 @@ Route::get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::post('/update/profile', [UserController::class, 'updateProfileImage']);
 });
 
 Route::post('/auth/register', [AuthController::class, 'register']);
